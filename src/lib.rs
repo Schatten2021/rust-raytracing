a4dcbb9bdbf15c6d6c29bac77724f17204cf6dea
Schatten2020 <schatten@mail.fms.nrw>
2025-03-20 12:32:39 +0100

    removed unnecessary code from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,3 @@ pub mod raytracing;
 pub use raytracing::camera::Camera;
 pub use raytracing::scene::{ Scene, Config };
 pub use raytracing::object;
-
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
